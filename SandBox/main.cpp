7fd7be3d4c886d173a5988f8aed0735372433ebb
samira <samiramhosseini19@gmail.com>
2023-07-24 19:09:07 -0500

    Refactored the code

--- SandBox/main.cpp
+++ SandBox/main.cpp
@@ -4,83 +4,96 @@
 // Person struct to hold data for each person.
 struct Person
 {
-    std::string name;
-    std::size_t age;
-    std::size_t ID;
 
-    // Constructor
-    Person(std::string_view name_, std::size_t age_, std::size_t ID_) :
-        name(name_), age(age_), ID(ID_)
-    {
-    }
 
-    // Operator overload for less than, used for default sorting by age.
-    bool operator< (const Person& rhs) const
-    {
-        return this->age < rhs.age;
-    }
 
-    // Getter for age
-    std::size_t getAge() const
-    {
-        return this->age;
-    }
+	// Constructor
+	Person(std::string_view name_, std::size_t age_, std::size_t ID_) :
+		name(name_), age(age_), ID(ID_)
+	{
+	}
 
-    // Getter for name
-    std::string getName() const
-    {
-        return this->name;
-    }
+	// Operator overload for less than, used for default sorting by age.
+	bool operator< (const Person& rhs) const
+	{
+		return this->age < rhs.age;
+	}
 
-    // Getter for ID
-    std::size_t getID() const
-    {
-        return this->ID;
-    }
+	// Getter for age
+	std::size_t getAge() const
+	{
+		return this->age;
+	}
+
+	// Getter for name
+	std::string getName() const
+	{
+		return this->name;
+	}
+
+	// Getter for ID
+	std::size_t getID() const
+	{
+		return this->ID;
+	}
+	// Data ---------
+private:
+	std::string name;
+	std::size_t age;
+	std::size_t ID;
 };
 
 // Custom comparison function for sorting by ID
 struct Comp
 {
-    bool operator()(const Person& lhs, const Person& rhs)
-    {
-        return lhs.getID() < rhs.getID();
-    }
+	bool operator()(const Person& lhs, const Person& rhs)
+	{
+		return lhs.getID() < rhs.getID();
+	}
 };
 
 int main()
 {
-    std::vector<Person> people{ {"Samira",19,1}, {"Mona",49,7}, {"Aida",55,3}, {"Pashmak",1,12} };
+	std::vector<Person> people
+	{ 
+		{"Sarah",19,1}, 
+		{"Jack",49,7}, 
+		{"Cameron",55,3}, 
+		{"Lili",10,12} 
+	};
 
-    // Sorting using operator < overload (by age)
-    std::cout << "-------------SORTED BY age---------------------------\n";
-    std::sort(people.begin(), people.end());
-    for (const auto& person : people)
-    {
-        std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
-    }
+	// Sorting using operator < overload (by age)
+	std::cout << "-------------SORTED BY age---------------------------\n";
+	std::sort(people.begin(), people.end());
+	for (const auto& person : people)
+	{
+		std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
+	}
 
-    // Sorting using custom comparison function (by ID)
-    std::cout << "--------------SORTED BY ID--------------------------\n";
-    std::ranges::sort(people, Comp());
-    for (const auto& person : people)
-    {
-        std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
-    }
 
-    // Sorting using lambda function (by name)
-    std::cout << "----------------SORTED BY name------------------------\n";
-    std::ranges::sort(people, [](const Person& lhs, const Person& rhs)->bool { return lhs.getName() < rhs.getName(); });
-    for (const auto& person : people)
-    {
-        std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
-    }
+	// Sorting using custom comparison function (by ID)
+	std::cout << "--------------SORTED BY ID--------------------------\n";
+	std::ranges::sort(people, Comp());
+	for (const auto& person : people)
+	{
+		std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
+	}
 
-    // Sorting using C++20 projection (by age)
-    std::cout << "----------------SORTED BY age using projection------------------------\n";
-    std::ranges::sort(people, {}, &Person::getAge);
-    for (const auto& person : people)
-    {
-        std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
-    }
+
+	// Sorting using lambda function (by name)
+	std::cout << "----------------SORTED BY name------------------------\n";
+	std::ranges::sort(people, [](const Person& lhs, const Person& rhs)->bool { return lhs.getName() < rhs.getName(); });
+	for (const auto& person : people)
+	{
+		std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
+	}
+
+
+	// Sorting using C++20 projection (by age)
+	std::cout << "----------------SORTED BY age using projection---------\n";
+	std::ranges::sort(people, {}, &Person::getAge);
+	for (const auto& person : people)
+	{
+		std::cout << person.getName() << " : " << person.getAge() << " : " << person.getID() << '\n';
+	}
 }
